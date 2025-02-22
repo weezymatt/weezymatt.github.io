ad508eb527ec2e17cb5d50afb0b373da37a70b94
weezymatt <mhernan0697@gmail.com>
2025-02-22 21:12:43 +0000

    Deploying to gh-pages from @ weezymatt/weezymatt.github.io@178ebe488d87336c2d0b2debeacb7040624685ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -389,16 +389,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-debuting-the-project-bridging-the-digital-divide-with-xri-global-and-ua-interns-at-the-lt4all-2025-conference-sparkles-smile",
+          title: 'Debuting the project “Bridging the Digital Divide” with XRI Global and UA interns...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
